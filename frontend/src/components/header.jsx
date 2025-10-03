64bcc8ce4f0065d99aca364b69ea95760445530f
katag1001 <katarinag1001@gmail.com>
2025-10-03 17:54:02 +0200

    yes

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,35 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import wearableLogo from '../assets/images/wearable_logo.png';
-import './header.css'
+import './header.css';
 
 const Header = ({ loggedIn }) => {
   return (
     <header className="header">
       <nav className="navbar">
         <div className="navbar-left">
-          <img src={wearableLogo} alt="Wearable Logo" className="navbar-logo" />
+          <Link to="/">
+            <img src={wearableLogo} alt="Wearable Logo" className="navbar-logo" />
+          </Link>
         </div>
 
         <div className="navbar-right">
           {loggedIn ? (
             <>
-            <Link to="/user" className="nav-link">User Page</Link>
-              <select
-                className="nav-select"
-                onChange={(e) => {
-                  const path = e.target.value;
-                  if (path) window.location.href = path;
-                }}
-              >
-                <option value="">Select Page</option>
-                <option value="/clothes">Clothes</option>
-                <option value="/matches">Matches</option>
-                <option value="/today-outfits">Today’s Outfits</option>
-                <option value="/">Homepage</option>
-              </select>
-
-              
+              <Link to="/user" className="nav-link">User Page</Link>
+              <Link to="/clothes" className="nav-link">Clothes</Link>
+              <Link to="/matches" className="nav-link">Matches</Link>
+              <Link to="/today-outfits" className="nav-link">Today’s Outfits</Link>
             </>
           ) : (
             <div className="auth-links">
