832838179446f5d0637fb7f4e1d2129c4197b1a2
katag1001 <katarinag1001@gmail.com>
2025-10-08 13:34:49 +0200

    deploy

--- src/components/today/viewToday.jsx
+++ src/components/today/viewToday.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './ViewToday.css';
+import './viewToday.css';
 import {URL} from "../../config"; 
 
 const ViewToday = () => {
